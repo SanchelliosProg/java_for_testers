4c6a586412b5d531c2b5618f06d6d1f4dfa49b74
SanchelliosProg <vasyania2009@gmail.com>
2017-03-11 20:10:37 +0300

    - ex 2

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Point p1 = new Point(23, 56);
+        Point p2 = new Point(8, 87);
+        System.out.println(String.valueOf(calculateDistanceBetween(p1, p2)));
+    }
+
+    private static double calculateDistanceBetween(Point a, Point b){
+        double x = b.getX() - a.getX();
+        double y = b.getY() - a.getY();
+        return Math.sqrt(x*x + y*y);
     }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Александр on 11.03.2017.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
