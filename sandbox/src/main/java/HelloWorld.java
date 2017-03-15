6bdd5cff16ba3f407889ffa8ea888801e2b096f0
SanchelliosProg <vasyania2009@gmail.com>
2017-03-15 11:14:52 +0300

    - исправление задания 2

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -4,11 +4,22 @@ public class HelloWorld {
         Point p1 = new Point(23, 56);
         Point p2 = new Point(8, 87);
         System.out.println(String.valueOf(calculateDistanceBetween(p1, p2)));
+
+        Point p3 = new Point(45, 98);
+        print(p3.getDistance(p1));
+        print(Point.getDistance(p2, p3));
     }
 
     private static double calculateDistanceBetween(Point a, Point b){
         double x = b.getX() - a.getX();
         double y = b.getY() - a.getY();
         return Math.sqrt(x*x + y*y);
+    }
+
+    private static void print(Object ... object) {
+        for (Object o : object) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
     }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -17,4 +17,18 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    public double getDistance(Point point){
+        return calculateDistance(this, point);
+    }
+
+    public static double getDistance(Point p1, Point p2){
+        return calculateDistance(p1, p2);
+    }
+
+    private static double calculateDistance(Point p1, Point p2) {
+        double x = p1.getX() - p2.getX();
+        double y = p1.getY() - p2.getY();
+        return Math.sqrt(x*x + y*y);
+    }
 }
