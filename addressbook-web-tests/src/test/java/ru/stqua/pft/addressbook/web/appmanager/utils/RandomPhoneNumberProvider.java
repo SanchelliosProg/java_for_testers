0eab3191765a77baaf089608daba35dc9614e924
SanchelliosProg <vasyania2009@gmail.com>
2017-04-21 09:10:26 +0300

    - phone randomizer

---
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/web/appmanager/utils/RandomPhoneNumberProvider.java
@@ -0,0 +1,16 @@
+package ru.stqua.pft.addressbook.web.appmanager.utils;
+
+import java.util.Random;
+
+/**
+ * Created by Александр on 21.04.2017.
+ */
+public class RandomPhoneNumberProvider {
+    public static String generateRandomNumber(){
+        Random rnd = new Random();
+        int country = rnd.nextInt(10);
+        int city = rnd.nextInt(1000 - 100) + 100;
+        int phone = rnd.nextInt(10000000 - 1000000) + 1000000;
+        return String.valueOf(country + " (" + city + ") - " + phone);
+    }
+}
